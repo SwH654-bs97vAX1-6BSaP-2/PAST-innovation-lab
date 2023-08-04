36899cd5100c00733ef46b343db6e06924339cf1
mamasitta <vikiber2013@gmail.com>
2023-08-04 14:49:53 +0200

    updates 4/08

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,30 @@ WA.onInit().then(() => {
         WA.room.showLayer("facade-furniture-bg");
         WA.room.showLayer("sign");
       });
+      WA.room.onEnterLayer("floor1").subscribe(() => {
+        WA.room.hideLayer("roof1");
+        WA.room.hideLayer("walls-bg-front1");
+        WA.room.hideLayer("sign1");
+      });
+      
+    WA.room.onLeaveLayer("floor1").subscribe(() => {
+        WA.room.showLayer("roof1");
+        WA.room.showLayer("walls-bg-front1");
+        WA.room.showLayer("facade-furniture-bg1");
+        WA.room.showLayer("sign1");
+      });
+      WA.room.onEnterLayer("floor2").subscribe(() => {
+        WA.room.hideLayer("roof2");
+        WA.room.hideLayer("walls-bg-front2");
+        WA.room.hideLayer("sign2");
+      });
+      
+    WA.room.onLeaveLayer("floor2").subscribe(() => {
+        WA.room.showLayer("roof2");
+        WA.room.showLayer("walls-bg-front2");
+        WA.room.showLayer("facade-furniture-bg2");
+        WA.room.showLayer("sign2");
+      });
 
       WA.room.onEnterLayer("rooms_floor").subscribe(() => {
         WA.room.hideLayer("facade-furniture-fg");
